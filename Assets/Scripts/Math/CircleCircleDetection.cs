99602b55f6e37b3130b76adc6d35fa1ba7053d07
Joshua Hardwick <200560630@student.georgianc.on.ca>
2023-12-10 14:26:52 -0500

    part 1 complete

--- Assets/Scripts/Math/CircleCircleDetection.cs
+++ Assets/Scripts/Math/CircleCircleDetection.cs
@@ -1,37 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CircleCircleDetection : MonoBehaviour
 {
-    public GameObject circle1;
-    public GameObject circle2;
-    public GameObject cursor;
-    public GameObject projection;
+    public GameObject square;
+    public GameObject circle;
     public GameObject capsule;
     public GameObject capsuleTop;
     public GameObject capsuleBot;
+
 
     // Homework 10: complete the CheckCollisionCirleCapsule function.
     bool CheckCollisionCircleCapsule(
         Vector3 circlePosition, float circleRadius,
         Vector3 capsulePosition, float capsuleRadius, Vector3 direction, float halfHeight, out Vector3 mtv)
     {
-        // 1. Determine capsule top and bottom points.
-        // 2. Project the circle onto the line between capsule top and bottom.
-        // 3. Do circle-circle detection between the circle point & circle radius vs projected circle point & capsule radius.
 
-        // 1:
-        // Make top & bot vectors, call CapsulePoints to assign them.
+        Vector3 topCap;
+        Vector3 botCap;
 
-        // 2:
-        // Store projection of circle position onto capsule top & bot using ProjectPointLine.
+        CapsulePoints(capsulePosition, direction, halfHeight, out topCap, out botCap);
 
-        // 3:
-        // Use CheckCollisionCircles to compare the circle's position & radius vs the projection & capsule's radius.
+        Vector3 projection = ProjectPointLine(circlePosition, topCap, botCap);
+        return CheckCollisionCircles(circlePosition, circleRadius, projection, capsuleRadius, out mtv);
 
-        mtv = Vector3.zero;
-        return false;
+    }
+    bool CheckCollisionSquareCapsule(Vector3 sPos, float sRad, Vector3 capPos, float capRad,
+            Vector3 capDir, float HalfHeight, out Vector3 mtv)
+    {
+        Vector3 topCap;
+        Vector3 botCap;
+        CapsulePoints(capPos, capDir, HalfHeight, out topCap, out botCap);
+        return CheckCollisionSquareCircles(sPos, sRad, topCap, botCap, capRad, out mtv);
     }
 
     void CapsulePoints(Vector3 position, Vector3 direction, float halfHeight, out Vector3 top, out Vector3 bot)
@@ -48,6 +50,85 @@ public class CircleCircleDetection : MonoBehaviour
         float t = Vector3.Dot(AB, AP) / Vector3.Dot(AB, AB);
         t = Mathf.Clamp(t, 0.0f, 1.0f);
         return A + AB * t;
+    }
+
+
+    bool CheckCollisionSquareCircles(Vector3 position1, float radius1, Vector3 topCap, Vector3 botCap, float radius2, out Vector3 mtv)
+    {
+
+        Vector3 tleft = position1;
+        tleft.x -= radius1;
+        tleft.y += radius1;
+        Vector3 tright = position1;
+        tright.x += radius1;
+        tright.y += radius1;
+        Vector3 bleft = position1;
+        bleft.x -= radius1;
+        bleft.y -= radius1;
+        Vector3 bright = position1;
+        bright.x += radius1;
+        bright.y -= radius1;
+
+        // Distance between position 1 and position 2
+        float distancetleft = (topCap - tleft).magnitude;
+        float distancetright = (topCap - tright).magnitude;
+        float distancebleft = (topCap - bleft).magnitude;
+        float distancebright = (topCap - bright).magnitude;
+
+        float distancetleftbot = (botCap - tleft).magnitude;
+        float distancetrightbot = (botCap - tright).magnitude;
+        float distancebleftbot = (botCap - bleft).magnitude;
+        float distancebrightbot = (botCap - bright).magnitude;
+
+        // Collision if distance between circles is less than the sum of their radii!
+        bool collision;
+        if (distancetleft <= radius2)
+        {
+            collision = true;
+        }
+        else if (distancetright <= radius2)
+        {
+            collision = true;
+        }
+        else if(distancebleft <= radius2)
+        {
+            collision = true;
+        }
+        else if(distancebright <= radius2)
+        {
+            collision = true;
+        }
+        else if (distancetleftbot <= radius2)
+        {
+            collision = true;
+        }
+        else if (distancetrightbot <= radius2)
+        {
+            collision = true;
+        }
+        else if (distancebleftbot <= radius2)
+        {
+            collision = true;
+        }
+        else if (distancebrightbot <= radius2)
+        {
+            collision = true;
+        }
+        else if((botCap - position1).magnitude <= radius1 + radius2)
+        {
+            collision = true;
+        }
+        else if ((topCap - position1).magnitude <= radius1 + radius2)
+        {
+            collision = true;
+        }
+        else
+        {
+            collision = false;
+        }
+        mtv = Vector3.zero;
+
+        return collision;
     }
 
     bool CheckCollisionCircles(Vector3 position1, float radius1, Vector3 position2, float radius2, out Vector3 mtv)
@@ -78,33 +159,24 @@ public class CircleCircleDetection : MonoBehaviour
 
     void Update()
     {
-        // Get mouse position in screen-space, then convert it to world-space
-        Vector3 mouse = Input.mousePosition;
-        mouse = Camera.main.ScreenToWorldPoint(mouse);
-        mouse.z = 0.0f;
-        cursor.transform.position = mouse;
 
-        Vector2 position1 = circle1.transform.position;
-        Vector2 position2 = circle2.transform.position;
-        float radius1 = circle1.transform.localScale.x * 0.5f;
-        float radius2 = circle2.transform.localScale.x * 0.5f;
+        Vector2 position1 = square.transform.position;
+        float radius1 = square.transform.localScale.x * 0.5f;
+        Vector2 position2 = circle.transform.position;
+        float radius2 = circle.transform.localScale.x * 0.5f;
 
         // MTV resolves 1 from 2
         Vector3 mtv = Vector3.zero;
         bool collision = CheckCollisionCircles(position1, radius1, position2, radius2, out mtv);
-        circle1.transform.position += new Vector3(mtv.x, mtv.y, 0.0f);
+        //square.transform.position += new Vector3(mtv.x, mtv.y, 0.0f);
         Color color = collision ? Color.green : Color.red;
 
         //Vector3 A = mouse;
         //Vector3 B = position1 - Vector2.zero;
         //Vector3 proj = Vector3.Project(A, B);
-        Debug.DrawLine(Vector3.zero, mouse, Color.blue);
-        Debug.DrawLine(Vector3.zero, position1, Color.red);
-        Vector3 proj = ProjectPointLine(mouse, position1, Vector3.zero);
-        projection.transform.position = proj;
 
-        circle1.GetComponent<SpriteRenderer>().color = color;
-        circle2.GetComponent<SpriteRenderer>().color = color;
+        square.GetComponent<SpriteRenderer>().color = color;
+        circle.GetComponent<SpriteRenderer>().color = color;
 
         Vector3 capsulePosition = capsule.transform.position;
         Vector3 capsuleDirection = capsule.transform.up;
@@ -115,31 +187,31 @@ public class CircleCircleDetection : MonoBehaviour
         capsuleTop.transform.position = top;
         capsuleBot.transform.position = bot;
 
-        bool capsuleCollision = CheckCollisionCircleCapsule(position1, radius1,
+        bool capsuleCircleCollision = CheckCollisionCircleCapsule(position2, radius2,
             capsulePosition, capsuleRadius, capsuleDirection.normalized, capsuleHalfHeight, out mtv);
-        circle1.transform.position += mtv;
-        if (capsuleCollision)
+        //square.transform.position += mtv;
+        if (capsuleCircleCollision)
         {
             capsule.GetComponent<SpriteRenderer>().color = Color.green;
+            circle.GetComponent<SpriteRenderer>().color = Color.green;
+
+        }
+        else
+        {
+            capsule.GetComponent<SpriteRenderer>().color = Color.red;
+        }
+
+        bool capsuleSquareCollision = CheckCollisionSquareCapsule(position1, radius1, capsulePosition, capsuleRadius,
+            capsuleDirection.normalized, capsuleHalfHeight, out mtv);
+        if (capsuleSquareCollision)
+        {
+            capsule.GetComponent<SpriteRenderer>().color = Color.green;
+            square.GetComponent<SpriteRenderer>().color = Color.green;
+
         }
         else
         {
             capsule.GetComponent<SpriteRenderer>().color = Color.red;
         }
     }
-
-    // Homework 7 solution
-    /*
-    bool CheckCollisionCircles(Vector2 position1, float radius1, Vector2 position2, float radius2)
-    {
-        // 1. Calculate distance between position1 and position2
-        float distance = Vector2.Distance(position1, position2);
-
-        // 2. Calculate sum of radius1 + radius2
-        float radiiSum = radius1 + radius2;
-
-        // 3. Compare whether the distance is less than the radii sum (if so, there's a collision)!
-        return distance < radiiSum;
-    }
-    */
 }
